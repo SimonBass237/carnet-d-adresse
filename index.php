989f54136d94fc3d3f275ec267938033000900e7
SimonBass237 <83770192+SimonBass237@users.noreply.github.com>
2021-05-30 23:33:26 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+// session_start();
+// if(!$_SESSION['mail']){
+//     header( 'Location: admin_login.php');
+// }
+// include_once('../bd.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Carnet d'adresse</title>
+</head>
+<body>
+<form action=""></form>
+    <div class="  bg_img text_align_center">
+        <nav class="row  justify_around " style="">
+            <ul class="col-12 padd_marg">
+                <h1>Bienvenue dans ton carnet d'adresse</h1>
+            </ul>
+            <ul class="row col-12 justify_between">                             
+                <li class="col-12  padd_marg"><a class="button" href="php/ajout.php">Nouvelle adresse</a></li>
+                <li class="col-12  padd_marg"><a class="button" href="php/consulter.php">Consulter </a></li>   
+            </ul> 
+        </nav>
+    </div>
+</body>
+</html>
